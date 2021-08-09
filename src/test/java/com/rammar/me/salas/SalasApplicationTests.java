a4e5b08f04afcac6f43b1e831521c209fb2f04b0
Ramar <rtawera@gmail.com>
2021-08-09 10:00:48 -0300

    First commit

---
+++ src/test/java/com/rammar/me/salas/SalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rammar.me.salas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

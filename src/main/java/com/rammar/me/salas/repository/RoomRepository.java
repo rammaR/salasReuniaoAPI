654ae59e7da579bceafd7ece412e7f821e51fef3
Ramar <rtawera@gmail.com>
2021-08-09 13:50:50 -0300

    Remoção dos arquivos na raiz

--- src/main/java/com/rammar/me/salas/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rammar.me.salas.repository;
-
-import com.rammar.me.salas.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
